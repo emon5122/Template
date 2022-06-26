6e3e9a83f3255f59906e2dbab0574e6251bc90dc
Istiak Hassan Emon <emon5122@gmail.com>
2022-06-26 22:39:15 +0600

    Test

--- Dockerfiles/Workstation/init.sh
+++ Dockerfiles/Workstation/init.sh
@@ -7,3 +7,6 @@ git clone https://github.com/emon5122/Template.git
 apt install python3 python3-pip golang-go -y
 curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
 apt-get install -y nodejs
+ curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor |  tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" |  tee /etc/apt/sources.list.d/yarn.list
+apt-get update &&  apt-get install yarn
